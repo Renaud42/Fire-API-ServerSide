427b2910f05981e8bd6af3424693a1148e8a867b
Renaud42 <renaud.4243@gmail.com>
2018-09-21 19:33:08 +0200

    Little update for tests

--- wrapper/fire-api.php
+++ wrapper/fire-api.php
@@ -602,6 +602,7 @@ Public License instead of this License.  But first, please read
   $json_fireradio_stats = $fireradio_base_URL . "stats.json";
   $json_mumble = "https://panel.omgserv.com/json/180774/status";
   $json_vps = "https://panel.omgserv.com/json/180278/status";
+  $user_agent = $_SERVER['HTTP_USER_AGENT'];
   /***************************************************
   **                      MATH                      **
   ***************************************************/
@@ -773,7 +774,7 @@ Public License instead of this License.  But first, please read
         case 'mumble_online_players':
           return json_decode(file_get_contents($json_mumble), true)['status']['players']['online'];
           break;
-        case 'ram_max'
+        case 'ram_max':
           return json_decode(file_get_contents($json_vps), true)['status']['ram']['total'];
           break;
         case 'ram_percent':
@@ -794,9 +795,93 @@ Public License instead of this License.  But first, please read
           break;
       }
     }
+
+    /**
+     * Detect client browser.
+     */
+    public function getClientBrowser() {
+      global $user_agent;
+
+      $browser = "Not recognized browser";
+
+      $browser_array = array(
+                              '/msie/i'           => 'Internet Explorer',
+                              '/firefox/i'        => 'Firefox',
+                              '/safari/i'         => 'Safari',
+                              '/chrome/i'         => 'Chrome',
+                              '/edge/i'           => 'Edge',
+                              '/opera/i'          => 'Opera',
+                              '/netscape/i'       => 'Netscape',
+                              '/maxthon/i'        => 'Maxthon',
+                              '/konqueror/i'      => 'Konqueror',
+                              '/mobile/i'         => 'Handheld Browser',
+                              '/firebrowser/i'    => 'Fire-Browser',
+                              '/xiaomi/i'         => 'Mi Browser'
+                       );
+
+      foreach ($browser_array as $regex => $value)
+          if (preg_match($regex, $user_agent))
+              $browser = $value;
+
+      return $browser;
+    }
+
+    /**
+  	 * Detect client operating system.
+  	 */
+    public function getClientOS() {
+      global $user_agent;
+
+      $os_platform = "Not recognized operating system";
+
+      $os_array = array('/windows nt 10/i'      =>  'Windows 10',
+                        '/windows nt 6.3/i'     =>  'Windows 8.1',
+                        '/windows nt 6.2/i'     =>  'Windows 8',
+                        '/windows nt 6.1/i'     =>  'Windows 7',
+                        '/windows nt 6.0/i'     =>  'Windows Vista',
+                        '/windows nt 5.2/i'     =>  'Windows Server 2003/XP x64',
+                        '/windows nt 5.1/i'     =>  'Windows XP',
+                        '/windows xp/i'         =>  'Windows XP',
+                        '/windows nt 5.0/i'     =>  'Windows 2000',
+                        '/windows me/i'         =>  'Windows ME',
+                        '/win98/i'              =>  'Windows 98',
+                        '/win95/i'              =>  'Windows 95',
+                        '/win16/i'              =>  'Windows 3.11',
+                        '/macintosh|mac os x/i' =>  'Mac OS X',
+                        '/mac_powerpc/i'        =>  'Mac OS 9',
+                        '/linux/i'              =>  'Linux',
+                        '/ubuntu/i'             =>  'Ubuntu',
+                        '/iphone/i'             =>  'iPhone',
+                        '/ipod/i'               =>  'iPod',
+                        '/ipad/i'               =>  'iPad',
+                        '/android/i'            =>  'Android',
+                        '/blackberry/i'         =>  'BlackBerry',
+                        '/webos/i'              =>  'Mobile'
+                      );
+
+      foreach ($os_array as $regex => $value)
+        if (preg_match($regex, $user_agent))
+          $os_platform = $value;
+
+      return $os_platform;
+    }
+
+    /**
+   	 * Detect server operating system.
+   	 */
+    public function getServerOS() {
+      return php_uname('s');
+    }
   }
 
+  // Fire-Auth requests
+  /**
+   * Fire-Auth stuff
+   *
+   * @author Renaud42
+   * @since 1.0
+   */
   class FireAuth {
-
+    
   }
 ?>
